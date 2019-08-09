c68c3bc14485bc74fc38e1e391c4ab1758170ee2
Brandon <brandonjsu@gmail.com>
2019-08-09 12:38:04 -0500

    added database

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v7");
+mongoose.connect("mongodb+srv://Brandon:brandonwazhere77@cluster0-hx7ty.mongodb.net/test?retryWrites=true&w=majority");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
